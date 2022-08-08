f2fde71fe08b931112e145bf7190fbeb2cb63ce1
Burak <burakakbal@outlook.com.tr>
2022-08-08 17:01:00 +0300

    cam

--- lib/main.dart
+++ lib/main.dart
@@ -2,36 +2,30 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_yemek_tarifi/screens/authentication/authenticate.dart';
-import 'package:flutter_yemek_tarifi/screens/authentication/login.dart';
-import 'package:flutter_yemek_tarifi/screens/authentication/register.dart';
 import 'package:flutter_yemek_tarifi/screens/home/homeScreen.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+  //await Firebase.initializeApp();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme:ThemeData.light().copyWith(
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.transparent,
-          centerTitle: false,
-          systemOverlayStyle: SystemUiOverlayStyle.light,
-          elevation: 0,
-        )
-      ),
-      home: Authenticate(),
+      theme: ThemeData.light().copyWith(
+          appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.transparent,
+        centerTitle: false,
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+        elevation: 0,
+      )),
+      home: homeScreen(),
     );
   }
 }
-
--- lib/models/recipes.dart
+++ lib/models/recipes.dart
@@ -19,50 +19,56 @@ List<Recipe> recipes = [
       id: 1,
       title: "Tavuk Sote",
       size: "   3-4 kişilik       ",
-      description: "1.Soğanı, kapya ve yeşil biberi ince uzun olarak jülyen doğrayın. Domatesin kabuğunu soyun, çekirdeklerini alın ve küp küp doğrayın. Sarımsaığı da dilimleyin.",
+      description:
+          "1.Soğanı, kapya ve yeşil biberi ince uzun olarak jülyen doğrayın. Domatesin kabuğunu soyun, çekirdeklerini alın ve küp küp doğrayın. Sarımsaığı da dilimleyin.",
       image: "assets/images/tarif1.jpg",
       color: Color.fromARGB(255, 222, 123, 80)),
-      Recipe(
+  Recipe(
       id: 2,
       title: "Patlıcan Kapama",
       size: "   3-4 kişilik       ",
-      description: "Patlıcanları alacalı soyun. İnce ve uzun bir şekilde kesin. Dilerseniz tuzlu suda bekletin ve iyice kurulayın.",
+      description:
+          "Patlıcanları alacalı soyun. İnce ve uzun bir şekilde kesin. Dilerseniz tuzlu suda bekletin ve iyice kurulayın.",
       image: "assets/images/tarif2.jpg",
       color: Color.fromARGB(255, 185, 178, 45)),
-      Recipe(
+  Recipe(
       id: 3,
       title: "Kavurmalı Tost",
       size: "   1-2 kişilik       ",
-      description: "Kavurmayı ince ince dilimleyin. Kornişon turşuları uzunlamasına kesin. Tost ekmeklerinin  içerisine ince bir tabaka tereyağ sürün. Kavurmayı iki dilim tost ekmeğine eşit şekilde paylaştırın. Üzerine turşu dilimleri dizin.",
+      description:
+          "Kavurmayı ince ince dilimleyin. Kornişon turşuları uzunlamasına kesin. Tost ekmeklerinin  içerisine ince bir tabaka tereyağ sürün. Kavurmayı iki dilim tost ekmeğine eşit şekilde paylaştırın. Üzerine turşu dilimleri dizin.",
       image: "assets/images/tarif3.jpg",
       color: Color.fromARGB(255, 233, 180, 94)),
-      Recipe(
+  Recipe(
       id: 4,
       title: "Sodalı Börek",
       size: "   3-4 kişilik       ",
-      description: "Böreğin sosu için; yumurta, süt, yağ ve sodayı güzelce çırpın.Peynirleri ve ince kıyılmış maydanozu harmanlayın.Yufkanızı tezgaha bütünüyle serin. Fırça yardımıyla sostan her yerine eşit gelecek şekilde sürün.",
+      description:
+          "Böreğin sosu için; yumurta, süt, yağ ve sodayı güzelce çırpın.Peynirleri ve ince kıyılmış maydanozu harmanlayın.Yufkanızı tezgaha bütünüyle serin. Fırça yardımıyla sostan her yerine eşit gelecek şekilde sürün.",
       image: "assets/images/tarif4.jpg",
       color: const Color(0xFF3D82AE)),
-      Recipe(
+  Recipe(
       id: 5,
       title: "Yoğurt Çorbası",
       size: "3-4 kişilik          ",
-      description: "Orta boy bir tencerenin içerisine suyu dökün. İçine pirinçleri ekleyin. Kısık ateşte pirinçler yumuşayana kadar pişirin.",
+      description:
+          "Orta boy bir tencerenin içerisine suyu dökün. İçine pirinçleri ekleyin. Kısık ateşte pirinçler yumuşayana kadar pişirin.",
       image: "assets/images/tarif5.jpg",
       color: Color.fromARGB(255, 232, 211, 110)),
-      Recipe(
+  Recipe(
       id: 6,
       title: "Sulu Patates",
       size: "3-4 kişilik          ",
-      description: "Patatesleri güzelce yıkayıp kabuklarını soyduktan sonra iri küpler halinde doğrayıp biraz suda bekletin.Soğanı yemeklik doğrayın ve tencerede kızdırdığınız yağın üzerine ekleyip kavurun.",
+      description:
+          "Patatesleri güzelce yıkayıp kabuklarını soyduktan sonra iri küpler halinde doğrayıp biraz suda bekletin.Soğanı yemeklik doğrayın ve tencerede kızdırdığınız yağın üzerine ekleyip kavurun.",
       image: "assets/images/tarif6.jpg",
       color: Color.fromARGB(255, 155, 212, 248)),
-      Recipe(
+  Recipe(
       id: 7,
       title: "Köfteli Karnabahar",
       size: "3-4 kişilik          ",
-      description: "Kıymayı geniş bir kaba alın. Üzerine 1 çay kaşığı tuz, 1/2 çay kaşığı karabiber, toz kırmızı biber, kimyon, 1 yemek kaşığı su ve ekmek kırıntısını ekleyip güzelce yoğurun. Ceviz büyüklüğünde parçalar kopararak yuvarlayın ve köftelerinizi hazırlayın.",
+      description:
+          "Kıymayı geniş bir kaba alın. Üzerine 1 çay kaşığı tuz, 1/2 çay kaşığı karabiber, toz kırmızı biber, kimyon, 1 yemek kaşığı su ve ekmek kırıntısını ekleyip güzelce yoğurun. Ceviz büyüklüğünde parçalar kopararak yuvarlayın ve köftelerinizi hazırlayın.",
       image: "assets/images/tarif7.jpg",
       color: Color.fromARGB(255, 199, 192, 192)),
-
 ];
