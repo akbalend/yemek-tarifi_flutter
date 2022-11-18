f7d6142d2a48c321458a9397ebcfeaa3e150f93f
Burak <burakakbal@outlook.com.tr>
2022-11-18 18:23:35 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My First Project',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light().copyWith(
           appBarTheme: const AppBarTheme(
